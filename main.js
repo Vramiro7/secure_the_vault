1e716a6523679624124682d7e518f21eeee0f2b9
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-03-04 11:31:19 -0800

    created index.html, linked to main.js, assigned 3 variables and created alert window for user

---
+++ main.js
@@ -0,0 +1,22 @@
+// assign 3 variables for each calculation 
+// one for 10, one for 40, and one for 39 
+// for 10 we will do 9 + 1
+// for 40, we do 10 * 4
+// for 39 we will strNum 3 and 9 
+// create an alert to notify the user of the combination 
+
+const userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+const num1 = 9 + 1;// 10 
+console.log(9 + 1, "I am the sum of 9 + 1"); 
+
+const num2 = 10 * 4; //40 
+console.log(10 * 4, "I am the product of 10 * 4");
+
+const num3 = 78 / 2; //39
+console.log(78 / 2, "I am the quotient of 78 / 2")
+
+// const num3 = "3" + "9";
+// console.log("3" + "9", "I am 3 and 9 catenated")
+
+alert (`${userMessage} ${num1} - ${num2} - ${num3}`)
