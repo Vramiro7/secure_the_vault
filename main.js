21e2fb862f2ae6da6cb812f1128b5a892d2456df
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-03-04 11:42:23 -0800

    added more comments and pseudocode

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// create a variable for the user message 
 // assign 3 variables for each calculation 
 // one for 10, one for 40, and one for 39 
 // for 10 we will do 9 + 1
 // for 40, we do 10 * 4
-// for 39 we will strNum 3 and 9 
+// for 39, we will do 78 / 2
 // create an alert to notify the user of the combination 
 
 const userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
@@ -19,4 +20,5 @@ console.log(78 / 2, "I am the quotient of 78 / 2")
 // const num3 = "3" + "9";
 // console.log("3" + "9", "I am 3 and 9 catenated")
 
-alert (`${userMessage} ${num1} - ${num2} - ${num3}`)
+
+alert (`${userMessage} ${num1} - ${num2} - ${num3}`) // catenating all variables in oder to display them to the user 
